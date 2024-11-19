ac2231c837cdb5a7748d8b35c48fa5d6de7aaad9
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-19 12:42:11 -0500

    ajout de la maintenance

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const isMaintenance = process.env.REACT_APP_MAINTENANCE_MODE === 'true';
+
+  if (isMaintenance) {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Site en Construction 🚧</h1>
+          <p>Nous travaillons actuellement sur une nouvelle version du site.<br />
+            Revenez bientôt pour découvrir nos nouveautés !</p>
+          <a href="mailto:contact@alxmultimedia.com" className="button">Contactez-nous</a>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <div className="container">
-        <h1>Site en Construction 🚧</h1>
-        <p>Nous travaillons actuellement sur une nouvelle version du site.<br />
-          Revenez bientôt pour découvrir nos nouveautés !</p>
-        <a href="mailto:contact@alxmultimedia.com" className="button">Contactez-nous</a>
-      </div>
+      <h1>Bienvenue sur notre application !</h1>
+      {/* Le contenu principal de ton application */}
     </div>
   );
 }
