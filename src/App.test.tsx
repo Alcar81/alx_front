abe64eeea0403d36baa5a1033ea0f067fdc4ffc5
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-20 11:00:06 -0500

    mise à jour du script

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
 test('renders main container or maintenance mode', () => {
-  // Définir une valeur par défaut pour l'environnement de test
+  act(() => {
+    // Rendre le composant App
+    render(<App />);
+  });
+
+  // Vérifier si on est en mode maintenance
   const isMaintenanceMode =
     process.env.REACT_APP_MAINTENANCE_MODE === 'true' || false;
-
-  render(<App />);
 
   if (isMaintenanceMode) {
     // Vérifier la présence de l'élément pour le mode maintenance
