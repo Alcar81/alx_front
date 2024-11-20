10464bcf3b759be8d40c4e9bbefeaac38b5739ae
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-20 11:10:19 -0500

    mise à jour du script

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,10 @@
-// dev.alxmultimedia.com/frontend/config/config.js
+// src/config/config.js
 const config = {
-    REACT_APP_API_URL: "https://dev.alxmultimedia.com/api",
-    REACT_APP_WEBSITE_NAME: "alxmultimedia",
-    REACT_APP_ENABLE_DEBUG: true,
-    REACT_APP_FRONTEND_URL: "https://dev.alxmultimedia.com",
-    REACT_APP_MAINTENANCE_MODE: true, // Changez cette valeur pour activer/désactiver le mode maintenance
-  };
-  
-  export default config;
-  
+  REACT_APP_API_URL: process.env.REACT_APP_API_URL || "https://dev.alxmultimedia.com/api",
+  REACT_APP_WEBSITE_NAME: process.env.REACT_APP_WEBSITE_NAME || "alxmultimedia",
+  REACT_APP_ENABLE_DEBUG: process.env.REACT_APP_ENABLE_DEBUG === "true",
+  REACT_APP_FRONTEND_URL: process.env.REACT_APP_FRONTEND_URL || "https://dev.alxmultimedia.com",
+  REACT_APP_MAINTENANCE_MODE: process.env.REACT_APP_MAINTENANCE_MODE === "true", // Dynamique
+};
+
+export default config;
