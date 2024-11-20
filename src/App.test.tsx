812de68dfa9b45d87bfb9085b594f5d6d9deca51
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-20 10:53:28 -0500

    mise à jour du script

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,14 +3,18 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders main container or maintenance mode', () => {
+  // Définir une valeur par défaut pour l'environnement de test
+  const isMaintenanceMode =
+    process.env.REACT_APP_MAINTENANCE_MODE === 'true' || false;
+
   render(<App />);
 
-  const isMaintenanceMode = process.env.REACT_APP_MAINTENANCE_MODE === 'true';
-
   if (isMaintenanceMode) {
+    // Vérifier la présence de l'élément pour le mode maintenance
     const maintenanceElement = screen.getByTestId('maintenance-mode');
     expect(maintenanceElement).toBeInTheDocument();
   } else {
+    // Vérifier la présence de l'élément pour le mode normal
     const mainContainer = screen.getByTestId('main-container');
     expect(mainContainer).toBeInTheDocument();
   }
