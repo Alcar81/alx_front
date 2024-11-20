cb24fdc7287ab49a72141d938efe68e40e3ec488
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-20 12:05:55 -0500

    test maintenance true

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,11 @@
-// src/config/config.js
 const config = {
   REACT_APP_API_URL: process.env.REACT_APP_API_URL || "https://dev.alxmultimedia.com/api",
   REACT_APP_WEBSITE_NAME: process.env.REACT_APP_WEBSITE_NAME || "alxmultimedia",
-  REACT_APP_ENABLE_DEBUG: process.env.REACT_APP_ENABLE_DEBUG === "true",
   REACT_APP_FRONTEND_URL: process.env.REACT_APP_FRONTEND_URL || "https://dev.alxmultimedia.com",
-  REACT_APP_MAINTENANCE_MODE: process.env.REACT_APP_MAINTENANCE_MODE === "true", // Dynamique
+
+  // Valeurs par défaut hardcodées
+  REACT_APP_ENABLE_DEBUG: false, // true pour mode debug
+  REACT_APP_MAINTENANCE_MODE: true, // true pour maintenance 
 };
 
 export default config;
