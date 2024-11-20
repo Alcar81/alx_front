a6b9552b59de3614521a73d43610546848c5628a
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-19 21:27:52 -0500

    mode maintenance

---
+++ config/config.js
@@ -0,0 +1,11 @@
+// dev.alxmultimedia.com/frontend/config/config.js
+const config = {
+    REACT_APP_API_URL: "https://dev.alxmultimedia.com/api",
+    REACT_APP_WEBSITE_NAME: "alxmultimedia",
+    REACT_APP_ENABLE_DEBUG: true,
+    REACT_APP_FRONTEND_URL: "https://dev.alxmultimedia.com",
+    REACT_APP_MAINTENANCE_MODE: false, // Changez cette valeur pour activer/désactiver le mode maintenance
+  };
+  
+  export default config;
+  
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
+import config from '../config/config';
 
 function App() {
-  const isMaintenance = process.env.REACT_APP_MAINTENANCE_MODE === 'true';
+  const isMaintenance = config.REACT_APP_MAINTENANCE_MODE;
 
   if (isMaintenance) {
     return (
       <div className="App">
         <div className="container">
           <h1>Site en Construction 🚧</h1>
-          <p>Nous travaillons actuellement sur une nouvelle version du site.<br />
-            Revenez bientôt pour découvrir nos nouveautés !</p>
-          <a href="mailto:contact@alxmultimedia.com" className="button">Contactez-nous</a>
+          <p>
+            Nous travaillons actuellement sur une nouvelle version du site.<br />
+            Revenez bientôt pour découvrir nos nouveautés !
+          </p>
+          <a href="mailto:contact@alxmultimedia.com" className="button">
+            Contactez-nous
+          </a>
         </div>
       </div>
     );
@@ -20,9 +25,10 @@ function App() {
   return (
     <div className="App">
       <h1>Bienvenue sur notre application !</h1>
-      {/* Le contenu principal de ton application */}
+      {/* Le contenu principal de votre application */}
     </div>
   );
 }
 
 export default App;
+
