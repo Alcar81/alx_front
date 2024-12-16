ac915aa5bd6c3f3f91417b53066edbb18fe98965
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-12-16 13:50:12 -0500

    ajout étape 13

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ const config = {
   REACT_APP_API_URL: process.env.REACT_APP_API_URL || "https://dev.alxmultimedia.com/api",
   REACT_APP_WEBSITE_NAME: process.env.REACT_APP_WEBSITE_NAME || "alxmultimedia",
   REACT_APP_FRONTEND_URL: process.env.REACT_APP_FRONTEND_URL || "https://dev.alxmultimedia.com",
-
+  
   // Valeurs par défaut hardcodées
   REACT_APP_ENABLE_DEBUG: false, // "true" pour mode debug
   REACT_APP_MAINTENANCE_MODE: false, // "true" pour maintenance 
