66a5d99776460e11ecae4bf9cf871446884dcb08
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-01-23 19:21:28 -0500

    ajustement index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,18 +7,18 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 
 // Récupération du nonce injecté par le backend
-const nonce = (window as any).__NONCE__;
+const nonce = (window as any).__NONCE__ || "";
 
 if (!nonce) {
-  console.error(
-    "Aucun nonce trouvé. Assurez-vous que le backend injecte correctement le nonce dans le HTML avec le placeholder __NONCE__."
+  console.warn(
+    "Aucun nonce trouvé. Cela pourrait poser problème avec la politique CSP. Assurez-vous que le backend injecte correctement le nonce dans le HTML."
   );
 }
 
 // Création du cache Emotion avec le nonce
 const cache = createCache({
   key: "css",
-  nonce: nonce || "", // Utilise un nonce vide comme fallback
+  nonce: nonce,
 });
 
 const root = ReactDOM.createRoot(
