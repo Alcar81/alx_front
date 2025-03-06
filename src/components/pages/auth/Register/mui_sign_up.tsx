e789352e75ab93556c11d1e90551443b1bbdb6dc
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-03-06 07:37:14 -0500

    modification du chemin relatif pour les images

--- src/components/pages/auth/Register/mui_sign_up.tsx
+++ src/components/pages/auth/Register/mui_sign_up.tsx
@@ -10,9 +10,9 @@ import FormControl from "@mui/material/FormControl";
 import Link from "@mui/material/Link";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import AppTheme from "@/theme/AppTheme";
-import ColorModeSelect from "@/theme/ColorModeSelect";
-import { GoogleIcon } from "@/theme/CustomIcons";
+import AppTheme from "@/components/theme/AppTheme";
+import ColorModeSelect from "@/components/theme/ColorModeSelect";
+import { GoogleIcon } from "@/components/theme/CustomIcons";
 import { Card, AuthContainer } from "../styles/authStyles";
 import alxLogo from "@/components/images/logo/alx_logo_long.png";
 
