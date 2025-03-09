a21c592c20957bba6707fb547f1aa541181d0e17
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-03-08 19:41:44 -0500

    modification du login et register

--- src/components/pages/auth/Login/mui_sign_in.tsx
+++ src/components/pages/auth/Login/mui_sign_in.tsx
@@ -10,11 +10,11 @@ import FormControl from "@mui/material/FormControl";
 import Link from "@mui/material/Link";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import ForgotPassword from "../auth/ForgotPassword";
-import AppTheme from "../../theme/AppTheme";
-import ColorModeSelect from "../../theme/ColorModeSelect";
-import { GoogleIcon } from "../../theme/CustomIcons";
-import { Card, AuthContainer } from "../../styles/authStyles";
+import ForgotPassword from "../ForgotPassword";
+import AppTheme from "../../../../theme/AppTheme";
+import ColorModeSelect from "../../../../theme/ColorModeSelect";
+import { GoogleIcon } from "../../../../theme/CustomIcons";
+import { Card, AuthContainer } from "../../../../theme/styles/authStyles";
 import alxLogo from "../../../images/logos/alx_logo_long.png";
 
 export default function SignIn(props: { disableCustomTheme?: boolean }) {
