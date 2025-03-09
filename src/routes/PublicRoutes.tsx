986fcd51d77a8b9a7d872a9459188d1d2a5c247a
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-03-08 19:19:26 -0500

    modification du login et register

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -7,10 +7,12 @@ import Home from "../components/pages/Home/Home";
 import About from "../components/pages/About/About";
 import Contact from "../components/pages/Contact/Contact";
 
-import NotFound from "../components/pages/Notfound/NotFound";
-
 import Login from "../components/pages/auth/Login/mui_sign_in";
 import Register from "../components/pages/auth/Register/mui_sign_up";
+
+import NotFound from "../components/pages/Notfound/NotFound";
+
+
 
 
 const PublicRoutes: React.FC = () => {
