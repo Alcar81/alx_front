95f31fc769d2f22e454f5165e39e9ce250950c12
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-04-14 09:35:47 -0400

    ajustement du délai pour redirection de inscription vers connexion

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,5 +1,5 @@
 // 📌 src/contexts/UserContext.tsx
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 interface User {
   email: string;
