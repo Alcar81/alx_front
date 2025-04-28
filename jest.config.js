4b6163c2d493f73c4d1a6dafec6d4860f785854f
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-04-28 19:11:58 -0400

    ajustement utilisateurs

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,11 @@ module.exports = {
   testEnvironment: "jsdom",
   globals: {
     "ts-jest": {
-      tsconfig: "tsconfig.jest.json"
-    }
+      tsconfig: "../tsconfig.jest.json",
+    },
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
 };
