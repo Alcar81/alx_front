814fceb28c63f7b3a1876c5f13683e41c6412426
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-04-28 20:43:53 -0400

    ajustement utilisateurs

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,8 +4,8 @@ const config = {
   REACT_APP_API_URL: process.env.REACT_APP_API_URL || "",
   REACT_APP_FRONTEND_URL: process.env.REACT_APP_FRONTEND_URL || "",
   REACT_APP_WEBSITE_NAME: process.env.REACT_APP_WEBSITE_NAME || "",
-  REACT_APP_ENABLE_DEBUG: process.env.REACT_APP_ENABLE_DEBUG === "true",
   REACT_APP_MAINTENANCE_MODE: process.env.REACT_APP_MAINTENANCE_MODE === "true",
+  REACT_APP_ENABLE_DEBUG: process.env.REACT_APP_ENABLE_DEBUG === "true",
 };
 
 export default config;
