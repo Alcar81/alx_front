edb0dca3ce95c776866edd1c1f2caea4f5ccbfe5
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-04-28 20:41:31 -0400

    ajustement utilisateurs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
 // 📁 src/config/config.ts
 
-const getConfig = () => ({
-  API_URL: process.env.REACT_APP_API_URL || "",
-  FRONTEND_URL: process.env.REACT_APP_FRONTEND_URL || "",
-  WEBSITE_NAME: process.env.REACT_APP_WEBSITE_NAME || "",
-  ENABLE_DEBUG: process.env.REACT_APP_ENABLE_DEBUG === "true",
-  MAINTENANCE_MODE: process.env.REACT_APP_MAINTENANCE_MODE === "true",
-});
+const config = {
+  REACT_APP_API_URL: process.env.REACT_APP_API_URL || "",
+  REACT_APP_FRONTEND_URL: process.env.REACT_APP_FRONTEND_URL || "",
+  REACT_APP_WEBSITE_NAME: process.env.REACT_APP_WEBSITE_NAME || "",
+  REACT_APP_ENABLE_DEBUG: process.env.REACT_APP_ENABLE_DEBUG === "true",
+  REACT_APP_MAINTENANCE_MODE: process.env.REACT_APP_MAINTENANCE_MODE === "true",
+};
 
-export default getConfig;
+export default config;
