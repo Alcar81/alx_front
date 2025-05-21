8a27e66242eec4c8188b30a8c851a1a21a60b95c
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-05-21 19:13:36 -0400

    ajout bouton page panel bloc text et block image

--- src/__tests__/testConfigReal.ts
+++ src/__tests__/testConfigReal.ts
@@ -1,6 +1,6 @@
 // 📁 src/__tests__/testConfigReal.ts
-import config from "@/config/config";
-import { configSchema } from "@/config/configSchema";
+import config from "../config/config";
+import { configSchema } from "../config/configSchema";
 
 describe("Validation de la configuration réelle", () => {
   it("devrait valider correctement le fichier config.ts", () => {
